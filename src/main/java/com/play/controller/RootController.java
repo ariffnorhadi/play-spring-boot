7e73d89032684175139f4424732dde7e07b972df
Muhammad Ariff Bin Norhadi <ariffnorhadi@gmail.com>
2021-07-31 08:45:41 +0800

    complete Retrieve all todos operation

--- src/main/java/com/play/controller/RootController.java
+++ src/main/java/com/play/controller/RootController.java
@@ -1,19 +1,30 @@
 package com.play.controller;
 
 import com.play.model.Todo;
+import com.play.service.TodoService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class RootController {
+
+  private final TodoService todoService;
+
+  public RootController(TodoService todoService) {
+    this.todoService = todoService;
+  }
 
   @GetMapping
   public String root(Model model) {
 
     Todo newTodo = new Todo();
-
     model.addAttribute("newTodo", newTodo);
+
+    List<Todo> todoList = this.todoService.getAllTodos();
+    model.addAttribute("todoList", todoList);
     return "index";
   }
 
